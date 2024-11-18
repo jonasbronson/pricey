1245f1ec71d3c8b25e503e4295263005115c6540
Jonas Bronson <138813233+jonasbronson@users.noreply.github.com>
2024-11-17 22:11:03 -0600

    converting database to firebase

--- app/index.tsx
+++ app/index.tsx
@@ -2,46 +2,9 @@ import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import React = require("react");
 import { Text, View } from "react-native";
-import * as SQLite from 'react-native-sqlite-storage';
 import {useState, useEffect} from 'react';
 
-const db = SQLite.openDatabase(
-  {
-    name: 'MainDB',
-    createFromLocation: 1,
-  },
-  () => {},
-  error => { console.log(error) }
-);
-
 export default function Index() {
-
-  /*const db = SQLite.openDatabaseAsync('local.db');
-  useEffect(() => {
-    db.transaction(tx => {
-      tx.executeSql('CREATE TABLE IF NOT EXISTS Entry (EntryID INTEGER PRIMARY KEY AUTOINCREMENT, Amount REAL, EntryDate TEXT, IsPurchase TEXT);');
-  });
-
-
-
-}, []);*/
-
-  useEffect(() => {
-    createTable();
-    newEntry();
-  }, []);
-
-  const createTable = () => {
-    db.transaction(tx => {
-      tx.executeSql('CREATE TABLE IF NOT EXISTS Entry (EntryID INTEGER PRIMARY KEY AUTOINCREMENT, Amount REAL, EntryDate TEXT, IsPurchase TEXT);');
-    });
-  }
-
-  const newEntry = () => {
-    db.transaction(tx => {
-      tx.executeSql('INSERT INTO Entry (Amount, EntryDate, IsPurchase) VALUES (?,?,?)', [10.00, '2021-10-01', 'true']);
-    });
-  }
 
   return (
     <ThemedView
